bb461fada21c3383a8ffe271f3ef95f887e15080
Yustian <ray.yustian@gmail.com>
2019-06-19 18:26:59 +0700

    Initial Commit

---
+++ spam-detector.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 18:12:54 2019
+
+@author: Yustian
+"""
+
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('spambase.data').as_matrix()
+np.random.shuffle(data)
+
+X = data[:, :48]
+Y = data[:, -1]
+
+Xtrain = X[:-100,]
+Ytrain = Y[:-100,]
+Xtest = X[-100:,]
+Ytest = Y[-100:,]
+
+model = MultinomialNB()
+model.fit(Xtrain, Ytrain)
+print("Classification rate for NB:", model.score(Xtest, Ytest))
+
+from sklearn.ensemble import AdaBoostClassifier
+
+model = AdaBoostClassifier()
+model.fit(Xtrain, Ytrain)
+print("Classification rate for AdaBoost:", model.score(Xtest, Ytest))
